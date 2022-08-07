5fdebda54ca21e5a21f1335e68b9132d6ce3387e
KseniaKolpashchikova <KseniaKolpashchikova@yandex.ru>
2022-08-07 20:52:45 +0300

    Input variable

---
+++ KR/Program.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+string [] FirstArray = new string [5] {"Hello", "Goodbay", "ok", "23", "Russia"};
+string[] SecondArray = new string[FirstArray.Length];

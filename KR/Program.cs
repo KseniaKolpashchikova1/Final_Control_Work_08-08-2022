3b4c490cc3f724800b4904b7c91cfadd48e51cb4
KseniaKolpashchikova <KseniaKolpashchikova@yandex.ru>
2022-08-07 20:56:02 +0300

    Method Void SearchElements

--- KR/Program.cs
+++ KR/Program.cs
@@ -5,3 +5,16 @@ Clear();
 
 string [] FirstArray = new string [5] {"Hello", "Goodbay", "ok", "23", "Russia"};
 string[] SecondArray = new string[FirstArray.Length];
+
+void SearchElements (string [] FirstArray, string [] SecondArray)
+{
+   int count = 0;
+   for (int i = 0; i < FirstArray.Length; i++)
+   {
+    if (FirstArray[i].Length<=3)
+    {
+        SecondArray[count] = FirstArray[i];
+        count++;
+    }
+   }
+}
